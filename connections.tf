6c24a5a40cbbf23d40edf291ceff1a03e3803a87
Adron Hall <adronhall@gmail.com>
2018-04-03 12:39:43 -0700

    Networks and subdomains.

--- connections.tf
+++ connections.tf
@@ -9,8 +9,13 @@ provider "aws" {
 }
 
 provider "azurerm" {
-  subscription_id = "0"
-  client_id = "1"
-  client_secret = "2"
-  tenant_id = "3"
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
 }
+
+variable subscription_id {}
+variable client_id {}
+variable client_secret {}
+variable tenant_id {}
