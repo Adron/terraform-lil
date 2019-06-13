f156a11472191012d8c142614503915d2bee8fe3
Christopher Montes <chris@sapho.com>
2019-06-13 17:28:06 -0600

    Fix the way tags are declared
    https://github.com/hashicorp/terraform/issues/19325#issuecomment-437096913

--- resources.tf
+++ resources.tf
@@ -7,8 +7,7 @@ resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags {
+  tags = {
     Name = "terraform-aws-vpc-example-two"
   }
 }
-
